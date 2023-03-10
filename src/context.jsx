63f57ff6df9270179f8cff2b87f6a6669c620a12
Chris Mems <christopher.mems@.com>
2023-03-10 11:26:04 -0600

    fetch random data

--- src/context.jsx
+++ src/context.jsx
@@ -1,11 +1,23 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 
 const AppContext = React.createContext()
 
 
 const AppProvider = ({ children }) => {
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://randomuser.me/api')
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
+    fetchData()
+  }, [])
 
   return <AppContext.Provider value={{name: 'John', role: 'students'}}>
     {children}
