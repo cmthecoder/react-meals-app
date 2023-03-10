db4dc99d3c273f080506e46a99cb76408b7d263e
Chris Mems <christopher.mems@.com>
2023-03-10 13:16:48 -0600

    use axios to get an array of meals

--- src/context.jsx
+++ src/context.jsx
@@ -1,22 +1,27 @@
 import React, {useContext, useEffect} from 'react'
+import axios from 'axios'
 
 const AppContext = React.createContext()
+
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
+
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 
 const AppProvider = ({ children }) => {
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://randomuser.me/api')
-        const data = await response.json()
-        console.log(data)
-      } catch (error) {
-        console.log(error)
-      }
+  const fetchMeals = async (url) => {
+    try {
+      const {data} = await axios(url)
+      console.log(data)
+    } catch (error) {
+      console.log(error.response)
     }
+  }
 
-    fetchData()
+  useEffect(() => {
+
+    fetchMeals(allMealsUrl)
   }, [])
 
   return <AppContext.Provider value={{name: 'John', role: 'students'}}>
