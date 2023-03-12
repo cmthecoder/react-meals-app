383dbcc8ad344b10922d0bc9c29caa614e1f3194
Chris Mems <christopher.mems@.com>
2023-03-11 19:54:12 -0600

    edit functions to save favorites to local storage

--- src/context.jsx
+++ src/context.jsx
@@ -7,6 +7,16 @@ const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
 
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
+const getFavoritesFromLocalStorage = () => {
+  let favorites = localStorage.getItem('favorites')
+  if (favorites) {
+    favorites = JSON.parse(localStorage.getItem('favorites'))
+  } else {
+    favorites = []
+  }
+  return favorites
+}
+
 
 const AppProvider = ({ children }) => {
   const [meals, setMeals] = useState([])
@@ -14,7 +24,7 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('')
   const [showModal, setShowModal] = useState(false)
   const [selectedMeal, setSelectedMeal] = useState(null)
-  const [favorites, setFavorites] = useState([])
+  const [favorites, setFavorites] = useState(getFavoritesFromLocalStorage())
 
   const fetchMeals = async (url) => {
     setLoading(true)
@@ -59,11 +69,13 @@ const AppProvider = ({ children }) => {
     if(alreadyFavorite) return
     const updatedFavorites = [...favorites, meal]
     setFavorites(updatedFavorites)
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites))
   }
 
   const removeFromFavorites = (idMeal) => {
     const updatedFavorites = favorites.filter((meal) => meal.idMeal !== idMeal)
     setFavorites(updatedFavorites)
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites))
   }
 
   useEffect(() => {
